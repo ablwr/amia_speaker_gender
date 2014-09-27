d25c28a90daf18cb19486f68b543daf0a3f84551
ablwr <ashley.blewer@gmail.com>
2014-09-27 17:17:10 -0400

    close to getting names extracted but need to close the gap and kill some br strong tags

--- scrape.rb
+++ scrape.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 class AMIA_Scraper
   attr_reader :amia_url
-  attr_reader :flatiron_doc
+  attr_reader :amia_doc
 
   def initialize
     @amia_url = 'http://www.amiaconference.net/the-preliminary-program/'
@@ -11,27 +11,39 @@ class AMIA_Scraper
   end
 
   def name_grabber(amia_doc)
-    # Need to grab first word after every br tag in a p class
-    # first word = all letters before " " and after every \n
-    amia_doc.css('br').remove_class('strong').each{ |br| br.replace("\n") }
-    first_names = amia_doc.search("p").collect{|e| e.text.split(/\r?\n/) }.flatten!.collect {|e| e.scan(/^\w+/).first }.compact!
+    first_names = amia_doc.search("p").collect{|e| e.text.split(/\n/) }.flatten!
+    first_names.delete_if{|str| str.length > 50}
+    first_names.delete_if{|str| str.include?("AMIA") || str.include?("Chair") || str.include?("Speaker")}
+    first_names.delete_if{|str| str == " " || str == "."}
+    first_names.delete_if{|str| str[0].to_i.to_s == str[0]}
     return first_names
+  end
+
+  def data_organizer(first_names)
+    first_names.each_with_index do |name, i|
+      if name.length > 25
+        first_names.delete_at(i)
+      end
+    end
   end
 
 end
 
 # easy access to debugging in irb
-# require 'nokogiri'
-# require 'open-uri'
-# require 'sexmachine'
-# amia_url = 'http://www.amiaconference.net/the-preliminary-program/'
-# amia_doc = Nokogiri::HTML(open(amia_url).read)
-# amia_doc.css('br').each{ |br| br.replace("\n") }
-# first_names = amia_doc.search("p").collect{|e| e.text.split(/\r?\n/) }.flatten!.collect {|e| e.scan(/^\w+/).first }.compact!
-# return first_names
+require 'nokogiri'
+require 'open-uri'
+require 'sexmachine'
+amia_url = 'http://www.amiaconference.net/the-preliminary-program/'
+amia_doc = Nokogiri::HTML(open(amia_url).read)
+first_names = amia_doc.search("p").collect{|e| e.text.split(/\n/) }.flatten!
+first_names.delete_if{|str| str.length > 70}
+first_names.delete_if{|str| str.include?("AMIA") || str.include?("Chair") || str.include?("Speaker")}
+first_names.delete_if{|str| str == " " || str == "."}
+first_names.delete_if{|str| str[0].to_i.to_s == str[0]}
+return first_names
 
 
-    first_names.each do |name|
-      d = SexMachine::Detector.new(case_sensitive: false)
-      d.get_gender("name")
-    end
+# What about if the string is less than a certain # of characters?
+# AND remove strong tags
+# if male, here. if female, here. if other, manual screen?
+
