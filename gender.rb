ebd84f1075ae00e392757d43348d273cdad969fe
ablwr <ashley.blewer@gmail.com>
2014-09-27 19:07:56 -0400

    dividing names into arrays based on results

--- gender.rb
+++ gender.rb
@@ -4,6 +4,21 @@ require 'sexmachine'
 
 d = SexMachine::Detector.new(case_sensitive: false)
 
-scrape.each do |name|
+first_names.each do |name|
+  d = SexMachine::Detector.new(case_sensitive: false)
+
+  female = []
+  male = []
+  andy = []
+
+  name = name.scan(/^\w+/)[0].to_s
+
+  if d.get_gender("name") == :female
+    female.push(name)
+  elsif d.get_gender("name") == :male
+    male.push(name)
+  else
+    andy.push(name)
+  end
 
 end
--- scrape.rb
+++ scrape.rb
@@ -11,39 +11,36 @@ class AMIA_Scraper
   end
 
   def name_grabber(amia_doc)
+    amia_doc.search('strong').remove
+    amia_doc.css('br').remove_class('strong').each{ |br| br.replace("\n") }
     first_names = amia_doc.search("p").collect{|e| e.text.split(/\n/) }.flatten!
-    first_names.delete_if{|str| str.length > 50}
-    first_names.delete_if{|str| str.include?("AMIA") || str.include?("Chair") || str.include?("Speaker")}
-    first_names.delete_if{|str| str == " " || str == "."}
+
+    first_names.delete_if{|str| str.length > 70}
+    first_names.delete_if{|str| str.include?("AMIA") || str.include?("Chair") || str.include?("Speaker") || str.include?("|")}
+    first_names.delete_if{|str| str == " " || str == " " || str == "." || str == "" }
     first_names.delete_if{|str| str[0].to_i.to_s == str[0]}
-    return first_names
+    first_names
   end
 
-  def data_organizer(first_names)
-    first_names.each_with_index do |name, i|
-      if name.length > 25
-        first_names.delete_at(i)
-      end
-    end
-  end
 
-end
+# easy copy-paste access to debugging in irb
 
-# easy access to debugging in irb
 require 'nokogiri'
 require 'open-uri'
 require 'sexmachine'
 amia_url = 'http://www.amiaconference.net/the-preliminary-program/'
 amia_doc = Nokogiri::HTML(open(amia_url).read)
+
+amia_doc.search('strong').remove
+amia_doc.css('br').remove_class('strong').each{ |br| br.replace("\n") }
 first_names = amia_doc.search("p").collect{|e| e.text.split(/\n/) }.flatten!
+
 first_names.delete_if{|str| str.length > 70}
-first_names.delete_if{|str| str.include?("AMIA") || str.include?("Chair") || str.include?("Speaker")}
-first_names.delete_if{|str| str == " " || str == "."}
+first_names.delete_if{|str| str.include?("AMIA") || str.include?("Chair") || str.include?("Speaker") || str.include?("|")}
+first_names.delete_if{|str| str == " " || str == " " || str == "." || str == "" }
 first_names.delete_if{|str| str[0].to_i.to_s == str[0]}
-return first_names
+first_names
 
+# number is 231
 
-# What about if the string is less than a certain # of characters?
-# AND remove strong tags
-# if male, here. if female, here. if other, manual screen?
 
