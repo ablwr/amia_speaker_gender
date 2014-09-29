8734cfd4d447b453d1340daf18d8a045a34b9fd5
ablwr <ashley.blewer@gmail.com>
2014-09-29 15:24:59 -0400

    refactor

--- scrape.rb
+++ scrape.rb
@@ -12,10 +12,17 @@ class AMIA_Scraper
     amia_doc.css('br').remove_class('strong').each{ |br| br.replace("\n") }
     first_names = amia_doc.search("p").collect{|e| e.text.split(/\n/) }.flatten!
 
-    first_names.delete_if{|str| str.length > 70 ||
-      str.length < 5 ||
-      str.include?("AMIA") || str.include?("Chair") || str.include?("Speaker") || str.include?("|") ||
-      }
+    first_names.delete_if{|str| str.length > 70 || str.length < 5 || str.include?("AMIA") || str.include?("Chair") || str.include?("Speaker") || str.include?("|") || str.include("–") }
+
+    first_names.collect do |str|
+      str.gsub!(/Dr./, "")
+      if str.include?(",")
+        str.gsub!(/,.*/,"")
+      else
+        str
+      end
+    end
+
     first_names
   end
 
