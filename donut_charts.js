48ca56048ab16a03eee3432d03993a17716d0dfe
ablwr <ashley.blewer@gmail.com>
2014-10-05 15:48:44 -0400

    refactored to have one changing chart instead of five static charts

--- donut_charts.js
+++ donut_charts.js
@@ -1,92 +1,46 @@
-  var ctx = document.getElementById("myChart").getContext("2d");
-  var data = [
-    {
-        value: 118,
-        color:"pink",
-        highlight: "red",
-        label: "Ladies"
-    },
-    {
-        value: 108,
-        color: "lightblue",
-        highlight: "blue",
-        label: "Gents"
-    }
-    ]
-  var myChart = new Chart(ctx).Doughnut(data);
+var ctx = document.getElementById("myChart").getContext("2d");
+var data = [
+  {
+      value: 118,
+      color:"pink",
+      highlight: "red",
+      label: "Ladies"
+  },
+  {
+      value: 108,
+      color: "lightblue",
+      highlight: "blue",
+      label: "Gents"
+  }
+  ]
+var myChart = new Chart(ctx).Doughnut(data);
 
-// second
+$( "#data" ).click(function() {
+  myChart.segments[0].value = 118;
+  myChart.segments[1].value = 108;
+  myChart.update();
+});
 
-  var ctx = document.getElementById("myChart2").getContext("2d");
-  var data2 = [
-    {
-        value: 96,
-        color:"pink",
-        highlight: "red",
-        label: "Ladies"
-    },
-    {
-        value: 88,
-        color: "lightblue",
-        highlight: "blue",
-        label: "Gents"
-    }
-    ]
-  var myChart2 = new Chart(ctx).Doughnut(data2);
+$( "#data2" ).click(function() {
+  myChart.segments[0].value = 96;
+  myChart.segments[1].value = 88;
+  myChart.update();
+});
 
-  // third
+$( "#data3" ).click(function() {
+  myChart.segments[0].value = 96;
+  myChart.segments[1].value = 91;
+  myChart.update();
+});
 
-  var ctx = document.getElementById("myChart3").getContext("2d");
-  var data3 = [
-    {
-        value: 96,
-        color:"pink",
-        highlight: "red",
-        label: "Ladies"
-    },
-    {
-        value: 91,
-        color: "lightblue",
-        highlight: "blue",
-        label: "Gents"
-    }
-    ]
-  var myChart3 = new Chart(ctx).Doughnut(data3);
+$( "#data4" ).click(function() {
+  myChart.segments[0].value = 31.5;
+  myChart.segments[1].value = 37.9;
+  myChart.update();
+});
 
-  // fourth
-
-  var ctx = document.getElementById("myChart4").getContext("2d");
-  var data4 = [
-    {
-        value: 31.5,
-        color:"pink",
-        highlight: "red",
-        label: "Ladies"
-    },
-    {
-        value: 37.9,
-        color: "lightblue",
-        highlight: "blue",
-        label: "Gents"
-    }
-    ]
-  var myChart4 = new Chart(ctx).Doughnut(data4);
-
-    // fifth
-
-  var ctx = document.getElementById("myChart5").getContext("2d");
-  var data5 = [
-    {
-        value: 25.33,
-        color:"pink",
-        highlight: "red",
-        label: "Ladies"
-    },
-    {
-        value: 29.6,
-        color: "lightblue",
-        highlight: "blue",
-        label: "Gents"
-    }
-    ]
-  var myChart5 = new Chart(ctx).Doughnut(data5);
+$( "#data5" ).click(function() {
+  myChart.segments[0].value = 25.33;
+  myChart.segments[1].value = 29.6;
+  myChart.update();
+});
