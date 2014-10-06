c222c8f80e235f06751f45023d5f74946071b356
ablwr <ashley.blewer@gmail.com>
2014-10-06 11:45:02 -0400

    typo corrected in scrape

--- scrape.rb
+++ scrape.rb
@@ -10,7 +10,7 @@ amia_doc.search('strong').remove
 amia_doc.css('br').remove_class('strong').each{ |br| br.replace("\n") }
 first_names = amia_doc.search("p").collect{|e| e.text.split(/\n/) }.flatten!
 
-    first_names.delete_if{|str| str.length > 70 || str.length < 5 || str.include?("AMIA") || str.include?("Chair") || str.include?("Speaker") || str.include?("|") || str.include("–") }
+    first_names.delete_if{|str| str.length > 70 || str.length < 5 || str.include?("AMIA") || str.include?("Chair") || str.include?("Speaker") || str.include?("|") || str.include?("–") }
 
   first_names.collect do |str|
     str.gsub!(/Dr./, "")
