8b5c2b15868e0fab4058e41a0b61a5d807e83152
Ashley Blewer <ashley.blewer@gmail.com>
2016-10-21 20:21:23 -0400

    parsing was easier this year

--- scrape.rb
+++ scrape.rb
@@ -1,28 +1,4 @@
 # easy copy-paste access to debugging in irb
-
 require 'nokogiri'
-require 'open-uri'
-require 'sexmachine'
-amia_url = 'http://www.amiaconference.net/the-preliminary-program/'
-amia_doc = Nokogiri::HTML(open(amia_url).read)
-
-amia_doc.search('strong').remove
-amia_doc.css('br').remove_class('strong').each{ |br| br.replace("\n") }
-first_names = amia_doc.search("p").collect{|e| e.text.split(/\n/) }.flatten!
-
-    first_names.delete_if{|str| str.length > 70 || str.length < 5 || str.include?("AMIA") || str.include?("Chair") || str.include?("Speaker") || str.include?("|") || str.include?("–") }
-
-  first_names.collect do |str|
-    str.gsub!(/Dr./, "")
-
-    if str.include?(",")
-      str.gsub!(/,.*/,"")
-    else
-      str
-    end
-
-  end
-
-first_names
-
-
+amia_doc = Nokogiri::HTML(File.open("program.html"))
+first_names = amia_doc.search("li").collect{|e| e.text.split(" ").first }
